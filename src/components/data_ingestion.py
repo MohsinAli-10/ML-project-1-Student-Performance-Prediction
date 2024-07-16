b7404c7c209ade445ec7c31a3c99c1fa2bf915b3
Mohsin Ali <mohsin.ali.tele@gmail.com>
2024-07-16 19:51:32 +0500

    Data ingestion annotation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -31,24 +31,36 @@ class DataIngestion:
             df = pd.read_csv('notebook/data/stud.csv') 
             logging.info("Read the data set as a dataframe")
             
+            #ensuring that the directories for the three paths exist, otherwise the following code creates them
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok = True)
+            os.makedirs(os.path.dirname(self.ingestion_config.test_data_path), exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
 
+            # ensures that the raw data is saved in the raw_data_path as backup before manipulation
             df.to_csv(self.ingestion_config.raw_data_path, index = False, header = True)
 
             logging.info("Train Test Split Initiated")
+            # A particular value of the random_state will ensure that the splitting of the data is deterministic 
+            # ... with repeated execution of the code
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            
+            #index false tells the command not to store the indexes
+            # header true tells the command to store the header row in the first row 
             train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
             test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
 
             logging.info("Ingestion of the data is complete")
             
             return (
+                # paths are required for the data transformation operations
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e, sys) # raises custom exception with the error message e and sys as the system information
         
+#block is only executed when the script is run directly, and not when it is imported as a module in another script. 
+# This helps prevent unintended side effects when the script is imported.
 if __name__ == '__main__':
     obj=DataIngestion()
     obj.initiate_data_ingestion()
