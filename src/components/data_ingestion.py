c453f3a6b4abb54a294f109f9aa912a9fc3d75a4
Mohsin Ali <mohsin.ali.tele@gmail.com>
2024-07-17 21:39:22 +0500

    Data Transformation has been added, along with minor changes to data ingestion and utils

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 # Easy to define just the variables within a class with the dataclass decorator
 @dataclass
 class DataIngestionConfig:
@@ -31,7 +34,7 @@ class DataIngestion:
             df = pd.read_csv('notebook/data/stud.csv') 
             logging.info("Read the data set as a dataframe")
             
-            #ensuring that the directories for the three paths exist, otherwise the following code creates them
+            # ensuring that the directories for the three paths exist, otherwise the following code creates them
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok = True)
             os.makedirs(os.path.dirname(self.ingestion_config.test_data_path), exist_ok=True)
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
@@ -63,4 +66,7 @@ class DataIngestion:
 # This helps prevent unintended side effects when the script is imported.
 if __name__ == '__main__':
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
